b9d7dec58caac3f7a2b62d3e02dc65cc656df84f
Anjali Gupta <152984851+anjali76Codes@users.noreply.github.com>
2025-02-09 18:58:04 +0530

    api connected to frontend and middleware added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 // App.jsx
 import React from 'react';
 import SideDrawer from './components/SideDrawer';
+import LegalAdvice from './pages/LegalAdvice';
 
 function App() {
   return (
     <div>
       <SideDrawer />
+      <LegalAdvice/>
+
     </div>
   );
 }
